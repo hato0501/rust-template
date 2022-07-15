ed46d23ff8dfad9fd1c08da480a4f0dc7ae68082
hato0501 <kharadadev@gmail.com>
2022-07-15 04:43:02 +0000

    test proconio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use proconio::input;
+
 fn main() {
-    println!("Hello, world!");
+    input! {
+        a: i32,
+        b: i32,
+    }
+
+    let answer = if a % 2 == 0 || b % 2 == 0 { "Even" } else { "Odd" };
+    println!("{}", answer)
 }
